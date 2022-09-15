2eb99d913944ca1168f7e8bca5ea7e281878d03a
Nassembele <z597y16aBEz>
2022-09-16 02:07:58 +0500

    add buttons

---
+++ src/components/Icons/RightArrow.tsx
@@ -0,0 +1,23 @@
+export const RightArrow = () => {
+
+    return (
+        <svg width="30" height="30" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_1_284)">
+                <circle cx="256" cy="256" r="240" stroke="rgb(83, 86, 150)" stroke-width="32"/>
+                <rect x="254.225" y="312.735" width="128" height="48" rx="20" transform="rotate(-45 254.225 312.735)"
+                      fill="rgb(83, 86, 150)"/>
+                <rect x="287.941" y="166" width="128" height="48" rx="20" transform="rotate(45 287.941 166)"
+                      fill="rgb(83, 86, 150)"/>
+                <rect x="134.225" y="312.735" width="128" height="48" rx="20" transform="rotate(-45 134.225 312.735)"
+                      fill="rgb(83, 86, 150)"/>
+                <rect x="167.941" y="166" width="128" height="48" rx="20" transform="rotate(45 167.941 166)"
+                      fill="rgb(83, 86, 150)"/>
+            </g>
+            <defs>
+                <clipPath id="clip0_1_284">
+                    <rect width="512" height="512" fill="white"/>
+                </clipPath>
+            </defs>
+        </svg>
+    )
+};
--- src/modules/comicsDemonstration/Comics.tsx
+++ src/modules/comicsDemonstration/Comics.tsx
@@ -3,6 +3,8 @@ import {getComic} from "./Comics.thunk";
 import {useDispatch, useSelector} from "react-redux";
 import styles from './Comics.module.scss';
 import {openComic} from "./Comics.selectors";
+import {RightArrow} from "../../components/Icons/RightArrow";
+import {Logout} from "../../components/Icons/Logout";
 
 export const Comics = () => {
 
@@ -15,22 +17,37 @@ export const Comics = () => {
         dispatch(getComic('614')); //614 601
     }, []);
 
-    return ( //todo компонента комиксов
+    return (
         <div className={styles.wrapper}>
             <div className={styles.comic_wrapper}>
                 <div className={styles.comic}>
                     <p className={styles.comic_title}>
                         {currentComic?.safe_title}
                     </p>
-                    <img src={currentComic?.img}
-                         className={styles.comic_content}
-                         alt={currentComic?.alt}
-                    />
+                    <div className={styles.comic_with_arrow}>
+                        <div className={styles.left_arrow}>
+                            <RightArrow/>
+                        </div>
+                        <img src={currentComic?.img}
+                             className={styles.comic_content}
+                             alt={currentComic?.alt}
+                        />
+                        <div className={styles.right_arrow}>
+                            <RightArrow/>
+                        </div>
+                    </div>
+                    <p className={styles.date}>
+                        {currentComic?.day &&
+                        `${currentComic?.day}.${currentComic?.month}.${currentComic?.year}`}
+                    </p>
                 </div>
             </div>
             <div className={styles.transcript_wrapper}>
                 <div className={styles.transcript}>
                     {currentComic?.transcript}
+                </div>
+                <div className={styles.logout}>
+                    <Logout/>
                 </div>
             </div>
         </div>
